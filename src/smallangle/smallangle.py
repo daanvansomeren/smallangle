d5b646d2d6feecfb55e9d6728a8862a821bccbe0
daanvansomeren <daanvsomeren@gmail.com>
2024-11-15 16:11:30 +0100

    smallangle has input number of steps

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,13 +3,31 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def smallangle():
+    pass
 
+@smallangle.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="Number of steps between 0 and pi",
+    show_default=True,  # show default in help
+)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
+@smallangle.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="Number of steps between 0 and pi",
+    show_default=True,  # show default in help
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -17,4 +35,4 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    smallangle()
